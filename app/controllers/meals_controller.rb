671da4b3323e6775102e0dd99ab8bc886b7039b2
vlad <VladDaImpaler@gmail.com>
2021-08-09 10:40:14 -0400

    added commented code to use for GET from themealsDB.com

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,3 +1,4 @@
+require 'net/http'
 class MealsController < ApplicationController
 
   def index
@@ -7,11 +8,17 @@ class MealsController < ApplicationController
   end
 
   def create
+    # url = "https://www.themealdb.com/api/json/v1/1/random.php"
+    # response = Net::HTTP.get(URI(url))
+    # payload = JSON.parse(response)
+    # payload["provider"]="www.themealdb.com"
     #Use method to identify provider, and parse data accordingly
     if getProvider.match(/www.themealdb.com/)
       Meal.parseMealsDB(meal_params(Meal.MealsDB_params))
     end
     
+    
+    binding.pry
   end
 
   def update
