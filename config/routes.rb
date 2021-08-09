a08c95554dbc89eab1508166022b3cbb761dd456
vlad <VladDaImpaler@gmail.com>
2021-08-09 10:38:31 -0400

    Set routes to be RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/meals/' => "meals#index"
   get '/meals/:id' => "meals#show"
   post '/meals/new' => "meals#create"
-  put '/meals/update' => "meals#update"
-  delete '/meals/destroy' => "meals#destroy"
+  put '/meals/:id' => "meals#update"
+  delete '/meals/:id' => "meals#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
