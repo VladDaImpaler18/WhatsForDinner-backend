610249c6b3edc9ca7da505a4f512e2edb3663c92
Vlad <VladDaImpaler@gmail.com>
2021-08-10 02:57:00 -0400

    custom validator, checks if nil, and if array >= 3

---
+++ app/helpers/meals_validator.rb
@@ -0,0 +1,10 @@
+class MealsValidator < ActiveModel::Validator
+    def validate(record)
+        if options[:fields].any? do |field| 
+            if record[:field].nil? || record[:field].length < 3 
+                record.errors.add :errors, "There must be at least 3 #{field.capitalize}" 
+            end
+           end
+        end
+    end
+end
