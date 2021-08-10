b8339b3b91ec056a6aeb167c405fe508f3a8da65
Vlad <VladDaImpaler@gmail.com>
2021-08-10 04:11:33 -0400

    fixed error with local variable

--- app/helpers/meals_validator.rb
+++ app/helpers/meals_validator.rb
@@ -1,7 +1,7 @@
 class MealsValidator < ActiveModel::Validator
     def validate(record)
         if options[:fields].any? do |field| 
-            if record[:field].nil? || record[:field].length < 3 
+            if record[field].nil? || record[field].length < 3 
                 record.errors.add :errors, "There must be at least 3 #{field.capitalize}" 
             end
            end
